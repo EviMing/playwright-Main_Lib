c4f8cf96e7c5c38a61a5b5111847be890e6cfa96
伊薇·茗 <2368199809@qq.com>
2026-09-25 21:29:36 +0800

    Update exe_get_page.py

--- exe_get_page.py
+++ exe_get_page.py
@@ -1,6 +1,6 @@
-#作者：伊茗(微信：EviMing，QQ：2368199809，邮箱：2368199809@qq.com)
+#作者：伊茗(微信：EviMing, QQ：2368199809, 邮箱：2368199809@qq.com)
 #[伊茗的GitHub仓库] = 'https://github.com/EviMing/'
-    #此文件所处项目 = https://github.com/EviMing/playwright-Generated_page
+    #此文件所处项目 = 'https://github.com/EviMing/playwright-Main_Lib'
 
 from Generated_page import Generated_page
 from playwright.sync_api import sync_playwright
