ceba8a02ef56f88bf79d8171ee6092c929d35c09
伊薇·茗 <2368199809@qq.com>
2026-09-25 21:29:47 +0800

    Update Generated_page.py

--- Generated_page.py
+++ Generated_page.py
@@ -1,6 +1,6 @@
 #作者：伊茗(微信：EviMing, QQ：2368199809, 邮箱：2368199809@qq.com)
 #[伊茗的GitHub仓库] = 'https://github.com/EviMing/'
-    #此文件所处项目 = 'https://github.com/EviMing/playwright-Generated_page'
+    #此文件所处项目 = 'https://github.com/EviMing/playwright-Main_Lib'
 
 from playwright.sync_api import sync_playwright, BrowserContext, Locator, JSHandle
 from playwright_stealth import Stealth
